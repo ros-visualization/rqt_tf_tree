679b57c95147d6bcc03232daeb0312339ed7fa1c
Arne Hitzmann <arne.hitzmann@gmail.com>
2022-08-25 12:34:23 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 package_name = 'rqt_tf_tree'
 setup(
     name=package_name,
-    version='1.0.2',
+    version='1.0.4',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
