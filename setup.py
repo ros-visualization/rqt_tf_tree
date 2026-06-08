f8e0d007d06fbd592ca1d1d889246fb689c84b72
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-06-08 10:29:15 +0200

    Removed license warning (#61)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
